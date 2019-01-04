312ab729c287fd0c3f9c35cabe5a9e8f9046edf7
Unknown <igorjordany@gmail.com>
2019-01-04 13:15:20 -0300

    Jardim inserido e controle giroscopio

---
+++ ProtegendoATerraTCC/Assets/_Scripts/GyroController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class GyroController : MonoBehaviour {
+
+	public GameObject player;
+
+	void Start () 
+	{
+		Input.gyro.enabled = true;
+	}
+
+	void Update () 
+	{
+		player.transform.Rotate (0, -Input.gyro.rotationRateUnbiased.y, 0);
+	}
+}
