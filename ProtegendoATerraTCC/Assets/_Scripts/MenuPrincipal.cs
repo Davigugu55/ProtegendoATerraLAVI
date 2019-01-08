12dab8c95f374d74eb98b68deadcf899b676133f
Unknown <igorjordany@gmail.com>
2019-01-08 13:07:58 -0300

    Menu temporario

---
+++ ProtegendoATerraTCC/Assets/_Scripts/MenuPrincipal.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour {
+
+	public void IniciarJogo(){
+		SceneManager.LoadScene (1);
+	}
+	public void SairJogo(){
+		Debug.Log ("Sair");
+		Application.Quit ();
+	}
+}
