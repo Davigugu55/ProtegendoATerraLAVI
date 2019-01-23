7ab5cea7d789da3cf7c3e44357caabb9b849ca67
Unknown <igorjordany@gmail.com>
2019-01-22 23:30:39 -0300

    Menu Pause

---
+++ ProtegendoATerraTCC/Assets/_Scripts/ChargeVolumeLevels.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChargeVolumeLevels: MonoBehaviour {
+
+	public Slider thisSlider;
+	public float geralVolume;
+	public float dublagemVolume;
+	public float musicaVolume;
+	public float ambienteVolume;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void SetSpecificVolume(string whatValue){
+		float sliderValue = thisSlider.value;
+
+		if (whatValue == "Geral") {
+			geralVolume = thisSlider.value;
+			AkSoundEngine.SetRTPCValue ("GeralVolume", geralVolume);
+		}
+
+		if (whatValue == "Musica") {
+			musicaVolume = thisSlider.value;
+			AkSoundEngine.SetRTPCValue ("MusicaVolume", musicaVolume);
+		}
+
+		if (whatValue == "Dublagem") {
+			dublagemVolume = thisSlider.value;
+			AkSoundEngine.SetRTPCValue ("DublagemVolume", dublagemVolume);
+		}
+
+		if (whatValue == "Ambiente") {
+			ambienteVolume = thisSlider.value;
+			AkSoundEngine.SetRTPCValue ("AmbienteVolume", ambienteVolume);
+		}
+	}
+}
---
+++ ProtegendoATerraTCC/Assets/_Scripts/PauseMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+	
+	public GameObject pauseMenuUI;
+
+	public void resume(){
+		Time.timeScale = 1f;
+	}
+	public void pause(){
+		Time.timeScale = 0f;
+	}
+}
