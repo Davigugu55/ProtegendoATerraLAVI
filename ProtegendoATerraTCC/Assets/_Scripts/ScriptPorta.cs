7cf379078c3654c3445f7b02518012b1aab40e5f
Unknown <igorjordany@gmail.com>
2019-01-31 17:35:56 -0300

    NavMeshSurface dinamico e interface com acabamento

--- ProtegendoATerraTCC/Assets/_Scripts/ScriptPorta.cs
+++ ProtegendoATerraTCC/Assets/_Scripts/ScriptPorta.cs
@@ -2,24 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.AI;
+//using UnityEngine.AI;
 
 public class ScriptPorta : MonoBehaviour {
 
 	private Animator animator;
 	public Button botao;
-	public NavMeshSurface[] superficie;
+	//public NavMeshSurface surface;
 
 	void Start () {
-		superficie = GetComponents<NavMeshSurface> ();
 		animator = GetComponent<Animator> ();
 	}
+
 	void OnTriggerEnter(Collider other){
 		botao.onClick.AddListener(TaskOnClick);
 	}
 	void TaskOnClick()
 	{
 		animator.SetBool ("open", true);
+		//surface.BuildNavMesh ();
 		Debug.Log("You have clicked the button!");
 	}
 }
