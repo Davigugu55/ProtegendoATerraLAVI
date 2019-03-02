efa837ef2fbb2059253d6c847d114829d6e9e0e8
Unknown <igorjordany@gmail.com>
2019-03-02 19:58:17 -0400

    Prologo inserido

---
+++ ProtegendoATerraTCC/Assets/_Scripts/Dialogue.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour
+{
+    public Text CaixaTexto;
+    public string[] sentences;
+    //private int index;
+
+    private void Start()
+    {
+        StartCoroutine(dialogo());
+    }
+    IEnumerator dialogo()
+    {
+        foreach(string sentence in sentences)
+        {
+            Debug.Log(sentence);
+            CaixaTexto.text = sentence;
+            yield return new WaitForSeconds(5);
+        }
+    }
+}
--- ProtegendoATerraTCC/Assets/_Scripts/MenuPrincipal.cs
+++ ProtegendoATerraTCC/Assets/_Scripts/MenuPrincipal.cs
@@ -7,16 +7,20 @@ using UnityEngine.UI;
 public class MenuPrincipal : MonoBehaviour {
 
 	public void IniciarJogo(){
-		SceneManager.LoadScene (1);
+		SceneManager.LoadScene (2);
 	}
 	public void SairJogo(){
 		Debug.Log ("Sair");
 		Application.Quit ();
 	}
 	public void Creditos(){
-		SceneManager.LoadScene (2);
+		SceneManager.LoadScene (3);
 	}
 	public void RetornaMenuPrincipal(){
 		SceneManager.LoadScene (0);
 	}
+    public void Prologo()
+    {
+        SceneManager.LoadScene(1);
+    }
 }
